ee7d3bb591b6239d43edf990d71c4571b6da0397
Fabrice MUKUNZI <mukunzifabrice77@gmail.com>
2024-09-22 11:49:02 +0200

    [chore]: added quinoa for build

--- backend/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-ALTER DATABASE certificates_manager SET search_path TO certificates;
-
-INSERT INTO certificates.suppliers (names, city, created_at, updated_at) VALUES
-('Supplier One', 'City One', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Supplier Two', 'City Two', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
